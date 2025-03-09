1ee87025d548a67ae651d3268a27987ccf69dfc3
Kazi Mohibuzzaman <kazimohib51@gmail.com>
2025-03-09 15:28:20 +0530

    added search functionality for selecting category of food item

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,14 +3,17 @@ import "./FoodDisplay.css"
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
     const {food_list} = useContext(StoreContext)
   return (
     <div className='food-display' id='food-display'>
         <h2>Top Dishes for you</h2>
         <div className='food-display-list'>
             {food_list.map((item, index)=>{
-                return <FoodItem key={index}  index={index} id={item._id} name={item.name} image={item.image} price={item.price} description={item.description} />
+                if(category==="All" || category ===item.category){
+                    return <FoodItem key={index}  index={index} id={item._id} name={item.name} image={item.image} price={item.price} description={item.description} />
+                }
+                
             })}
         </div>
     </div>
