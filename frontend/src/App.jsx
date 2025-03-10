6bd7de43cec7c19cd5150f81ea7c92043d5599db
Kazi Mohibuzzaman <kazimohib51@gmail.com>
2025-03-10 22:47:10 +0530

    created  cart order details page and  order summary page to checkout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Cart from "./pages/cart/Cart";
 import PlaceOrder from "./pages/placeOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
+import LoginPopup from './components/LoginPopup/LoginPopup';
 const App = () => {
+  const [showLogin, setshowLogin] = useState(false)
   return (
-    <>
+    <>  
+    {showLogin?<LoginPopup setshowLogin = {setshowLogin}/>:<></>}
       <div className="app">
-        <Navbar />
+        <Navbar setshowLogin ={setshowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/placeorder" element={<PlaceOrder />} />
+          <Route path="/order" element={<PlaceOrder />} />
         </Routes>
       </div>
       <Footer />
--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./LoginPopup.css"
-const LoginPopup = () => {
+import { assets } from '../../assets/assets'
+const LoginPopup = ({ setshowLogin}) => {
+    const [currState, setcurrState] = useState("Sign Up")
   return (
-    <div>LoginPopup</div>
+    <div className='login-popup'>
+        <form  className='login-popup-container'>
+            <div className="login-popup-title">
+                <h2>{currState}</h2>
+                <img onClick={()=>setshowLogin(false)} src={assets.cross_icon} alt="" />
+            </div>
+            <div className="login-popup-inputs">
+                {currState==="Login"?<></> : <input type='text' placeholder='enter your name' required  />}
+                <input type="email" placeholder='enter email' required />
+                <input type="password" placeholder='enter password' required />
+            </div>
+            <button>{currState==="Sign Up"?"Create Account":"Login"}</button>
+            <div className="login-popup-condition">
+                <input type="checkbox" required />
+                <p>By continuing,i aggree to the terms of use & privacy policy.</p>
+            </div>
+            <p>Create a new Account? <span onClick={()=>setcurrState("Sign up")}>Click here</span></p>
+            <p>Already have an account? <span onClick={()=>setcurrState("Login")}>Click here</span></p>
+        </form>
+    </div>
   )
 }
 
